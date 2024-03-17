4e6170be9c74dc338961d0c44ded28f6810414fd
Adriano Miranda <adrjmiranda@gmail.com>
2024-03-17 19:59:54 -0300

    feat: added user profile page.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,40 @@
 import { Request, Response } from 'express';
+import { CustomSession } from '../interfaces/CustomSession';
+import { FlattenMaps } from 'mongoose';
+import { Types } from 'mongoose';
+import User from '../interfaces/User';
+import UserModel from '../models/UserModel';
 
 export default class UserController {
 	static dashboard(req: Request, res: Response) {
 		res.render('user/dashboard');
 	}
 
-	static profile(req: Request, res: Response) {
-		res.render('user/profile');
+	static async profile(req: Request, res: Response): Promise<void> {
+		const id = (req.session as CustomSession).user?.id;
+
+		if (id) {
+			try {
+				const user:
+					| (FlattenMaps<User> & {
+							_id: Types.ObjectId;
+					  })
+					| null = await UserModel.findById({ _id: id })
+					.select('-password')
+					.lean()
+					.exec();
+
+				if (user) {
+					res.render('user/profile', { user });
+				} else {
+					res.status(400).send('User not found!');
+				}
+			} catch (error) {
+				console.error('Error fetching user profile:', error);
+				res.status(500).send('Internal Server Error');
+			}
+		} else {
+			res.redirect('/user/login');
+		}
 	}
 }
